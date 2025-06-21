c41fb067642258149355654ec7ed4b40eadcc88f
黃稚鈞 <hihiLOL940705@gmail.com>
2025-06-22 00:19:09 +0800

    random event

--- src/main/java/com/example/DataCollection.java
+++ src/main/java/com/example/DataCollection.java
@@ -1,13 +1,90 @@
 package com.example;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
 
 public class DataCollection {
+    // 隨機數生成器
+    private static Random random = new Random();
+
+    // 用於存儲已經處理過的事件 ID，避免重複
+    HashSet<Integer> umaEventSet = new HashSet<>();
+    HashSet<Integer> cardEventSet = new HashSet<>();
+
     // MySQL 資料庫連線設定
     // 請根據實際情況修改以下參數
     private static String url = "jdbc:mysql://localhost:3306/uma_db";
     private static String user = "root";
     private static String password = "Sawandcamein0705";
+    
+    static class Event {
+        Integer event_id;
+        String event_name;
+        Integer uma_id;
+        String uma_name;
+        String category;
+        List<Choice> choices = new ArrayList<>();
+        
+    }
+
+    static class Choice {
+        Integer choice_id;
+        Integer event_id;
+        String choice_label;
+
+        Integer speed;
+        Integer stamina;
+        Integer strength;
+        Integer willpower;
+        Integer intelligence;
+        Integer hp;
+        Integer mood;
+        Integer skill_point;
+        String event_condition;
+
+        boolean is_random_value;
+        boolean is_random_attribute;
+        boolean is_random_block_training;
+    }
+
+    static class CardEvent {
+        Integer event_id;
+        String event_name;
+        Integer uma_id;
+        String uma_name;
+        String category;
+        List<CardChoice> choices = new ArrayList<>();
+    }
+
+    static class CardChoice {
+        Integer choice_id;
+        Integer event_id;
+        String choice_label;
+        Integer chain_index;
+
+        Integer speed;
+        Integer stamina;
+        Integer strength;
+        Integer willpower;
+        Integer intelligence;
+        Integer hp;
+        Integer mood;
+        Integer skill_point;
+        Integer character_favor;
+        String event_condition;
+
+        boolean is_random_value;
+        boolean is_random_attribute;
+        boolean is_random_block_training;
+        boolean is_end_chained_event;
+    }
 
     // 獲取指定 count 的 UMA 資料
     // 這個方法會查詢資料庫中的 uma_table 表，根據 count
@@ -43,9 +120,57 @@ public class DataCollection {
 
     // 獲取指定 UMA ID 的事件資料
     // 這個方法會查詢資料庫中的 events 表，根據 uma_id
-    public static void GetUmaEventById(int uma_id) {
-            // SQL 查詢語句
-            String sql = "SELECT * FROM events WHERE uma_id = ?;";
+    // 並返回一個 Event 物件的列表
+    public static List<Event> GetUmaEventsById(int uma_id) {
+        //init
+        List<Event> events = new ArrayList<>();
+
+        // SQL 查詢語句
+        String sql = "SELECT * FROM events WHERE uma_id = ? and category != \"外出事件\";";
+
+        try (
+            Connection conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement stmt = conn.prepareStatement(sql);
+        ) {
+            stmt.setInt(1, uma_id);
+
+            // 執行查詢
+            try (ResultSet rs = stmt.executeQuery()) {
+                boolean hasData = false;
+
+                while (rs.next()) {
+                    hasData = true;
+                    Event event = new Event();
+
+                    event.event_id = rs.getInt("event_id");
+                    event.event_name = rs.getString("event_name");
+                    event.uma_id = rs.getInt("uma_id");
+                    event.uma_name = rs.getString("uma_name");
+                    event.category = rs.getString("category");
+                    event.choices = GetUmaEventChoicesByEventId(event.event_id);
+
+                    events.add(event);
+                }
+                if (!hasData) {
+                    System.out.println("查無資料");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return events;
+    }
+    
+    // 獲取指定 UMA ID 的外出事件資料
+    // 這個方法會查詢資料庫中的 events 表，根據 uma_id
+    // 並返回一個 Event 物件的列表
+    public static List<Event> GetUmaOutdoorEventsById(int uma_id) {
+        //init
+        List<Event> outdoorEvents = new ArrayList<>();
+
+        // SQL 查詢語句
+            String sql = "SELECT * FROM events WHERE uma_id = ? and category = \"外出事件\";";
 
             try (
                 Connection conn = DriverManager.getConnection(url, user, password);
@@ -59,12 +184,15 @@ public class DataCollection {
 
                     while (rs.next()) {
                         hasData = true;
+                        Event event = new Event();
 
-                        int event_id = rs.getInt("event_id");
-                        String event_name = rs.getString("event_name");
-                        String uma_name = rs.getString("uma_name");
-                        String category = rs.getString("category");
-                        System.out.println("event_id: " + event_id + ", event_name: " + event_name + ", uma_name: " + uma_name + ", category: " + category);
+                        event.event_id = rs.getInt("event_id");
+                        event.event_name = rs.getString("event_name");
+                        event.uma_id = rs.getInt("uma_id");
+                        event.uma_name = rs.getString("uma_name");
+                        event.category = rs.getString("category");
+
+                        outdoorEvents.add(event);
                     }
                     if (!hasData) {
                         System.out.println("查無資料");
@@ -73,16 +201,17 @@ public class DataCollection {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-        }
-    
 
-    public static void GetUmaOutdoorEventById() {
-        ;
+        return outdoorEvents;
     }
 
     // 獲取指定事件 ID 的選項資料
     // 這個方法會查詢資料庫中的 event_choices 表，根據 event_id
-    public static void GetUmaEventChoiceByEventId(int event_id) {
+    // 並返回一個 Choice 物件的列表
+    public static List<Choice> GetUmaEventChoicesByEventId(int event_id) {
+        //init
+        List<Choice> choices = new ArrayList<>();
+
         // SQL 查詢語句
         String sql = "SELECT * FROM event_choices WHERE event_id = ?;";
 
@@ -98,29 +227,25 @@ public class DataCollection {
 
                 while (rs.next()) {
                     hasData = true;
+                    Choice choice = new Choice();
 
-                    int choice_id = rs.getInt("choice_id");
-                    String choice_label = rs.getString("choice_label");
-                    int speed = rs.getInt("speed");
-                    int stamina = rs.getInt("stamina");
-                    int strength = rs.getInt("strength");
-                    int willpower = rs.getInt("willpower");
-                    int intelligence = rs.getInt("intelligence");
-                    int hp = rs.getInt("hp");
-                    int mood = rs.getInt("mood");
-                    int skill_point = rs.getInt("skill_point");
-                    String event_condition = rs.getString("event_condition");
-                    Boolean is_random_value = rs.getBoolean("is_random_value");
-                    Boolean is_random_attribute = rs.getBoolean("is_random_attribute");
-                    Boolean is_random_block_training = rs.getBoolean("is_random_block_training");
-                    System.out.println("choice_id: " + choice_id + ", choice_label: " + choice_label +
-                            ", speed: " + speed + ", stamina: " + stamina + ", strength: " + strength +
-                            ", willpower: " + willpower + ", intelligence: " + intelligence +
-                            ", hp: " + hp + ", mood: " + mood + ", skill_point: " + skill_point +
-                            ", event_condition: " + event_condition +
-                            ", is_random_value: " + is_random_value +
-                            ", is_random_attribute: " + is_random_attribute +
-                            ", is_random_block_training: " + is_random_block_training);
+                    choice.choice_id = rs.getInt("choice_id");
+                    choice.event_id = rs.getInt("event_id");
+                    choice.choice_label = rs.getString("choice_label");
+                    choice.speed = rs.getInt("speed");
+                    choice.stamina = rs.getInt("stamina");
+                    choice.strength = rs.getInt("strength");
+                    choice.willpower = rs.getInt("willpower");
+                    choice.intelligence = rs.getInt("intelligence");
+                    choice.hp = rs.getInt("hp");
+                    choice.mood = rs.getInt("mood");
+                    choice.skill_point = rs.getInt("skill_point");
+                    choice.event_condition = rs.getString("event_condition");
+                    choice.is_random_value = rs.getBoolean("is_random_value");
+                    choice.is_random_attribute = rs.getBoolean("is_random_attribute");
+                    choice.is_random_block_training = rs.getBoolean("is_random_block_training");
+
+                    choices.add(choice);
                 }
                 if (!hasData) {
                     System.out.println("查無資料");
@@ -129,6 +254,8 @@ public class DataCollection {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        return choices;
     }
 
     // 獲取指定 count 的 UMA 卡片資料
@@ -165,7 +292,10 @@ public class DataCollection {
 
     // 獲取指定 UMA ID 的卡片事件資料
     // 這個方法會查詢資料庫中的 card_events 表，根據 uma_id
-    public static void GetCardEventByCardUmaId(int uma_id) {
+    public static List<CardEvent> GetCardEventByCardUmaId(int uma_id) {
+        //init
+        List<CardEvent> cardEvents = new ArrayList<>();
+
         // SQL 查詢語句
         String sql = "SELECT * FROM card_events WHERE uma_id = ?;";
 
@@ -181,11 +311,16 @@ public class DataCollection {
 
                 while (rs.next()) {
                     hasData = true;
+                    CardEvent cardEvent = new CardEvent();
 
-                    int event_id = rs.getInt("event_id");
-                    String uma_name = rs.getString("uma_name");
-                    String category = rs.getString("category");
-                    System.out.println("event_id: " + event_id + ", uma_name: " + uma_name + ", category: " + category);
+                    cardEvent.event_id = rs.getInt("event_id");
+                    cardEvent.event_name = rs.getString("event_name");
+                    cardEvent.uma_id = rs.getInt("uma_id");
+                    cardEvent.uma_name = rs.getString("uma_name");
+                    cardEvent.category = rs.getString("category");
+                    cardEvent.choices = GetCardEventChoiceByCardEventId(cardEvent.event_id);
+
+                    cardEvents.add(cardEvent);
                 }
                 if (!hasData) {
                     System.out.println("查無資料");
@@ -194,11 +329,17 @@ public class DataCollection {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        return cardEvents;
     }
 
-    // 獲取指定事件 ID 的卡片選擇資料
+    // 獲取指定事件 ID 的卡片選項資料
     // 這個方法會查詢資料庫中的 card_event_choices 表，根據 card_event_id
-    public static void GetCardEventChoiceByCardEventId(int card_event_id) {
+    // 並返回一個 CardChoice 物件的列表
+    public static List<CardChoice> GetCardEventChoiceByCardEventId(int card_event_id) {
+        //init
+        List<CardChoice> cardChoices = new ArrayList<>();
+
         // SQL 查詢語句
         String sql = "SELECT * FROM card_event_choices WHERE event_id = ?;";
 
@@ -214,36 +355,28 @@ public class DataCollection {
 
                 while (rs.next()) {
                     hasData = true;
+                    CardChoice cardChoice = new CardChoice();
 
-                    int choice_id = rs.getInt("choice_id");
-                    String choice_label = rs.getString("choice_label");
-                    int chain_index = rs.getInt("chain_index");
-                    int speed = rs.getInt("speed");
-                    int stamina = rs.getInt("stamina");
-                    int strength = rs.getInt("strength");
-                    int willpower = rs.getInt("willpower");
-                    int intelligence = rs.getInt("intelligence");
-                    int hp = rs.getInt("hp");
-                    int mood = rs.getInt("mood");
-                    int skill_point = rs.getInt("skill_point");
-                    int character_favor = rs.getInt("character_favor");
-                    String event_condition = rs.getString("event_condition");
-                    Boolean is_random_value = rs.getBoolean("is_random_value");
-                    Boolean is_random_attribute = rs.getBoolean("is_random_attribute");
-                    Boolean is_random_block_training = rs.getBoolean("is_random_block_training");
-                    Boolean is_end_chained_event = rs.getBoolean("is_end_chained_event");
-                    System.out.println("choice_id: " + choice_id + ", choice_label: " + choice_label +
-                            ", chain_index: " + chain_index + ", speed: " + speed +
-                            ", stamina: " + stamina + ", strength: " + strength +
-                            ", willpower: " + willpower + ", intelligence: " + intelligence +
-                            ", hp: " + hp + ", mood: " + mood + ", skill_point: " + skill_point +
-                            ", character_favor: " + character_favor +
-                            ", event_condition: " + event_condition +
-                            ", is_random_value: " + is_random_value +
-                            ", is_random_attribute: " + is_random_attribute +
-                            ", is_random_block_training: " + is_random_block_training +
-                            ", is_end_chained_event: " + is_end_chained_event);
+                    cardChoice.choice_id = rs.getInt("choice_id");
+                    cardChoice.event_id = rs.getInt("event_id");
+                    cardChoice.choice_label = rs.getString("choice_label");
+                    cardChoice.chain_index = rs.getInt("chain_index");
+                    cardChoice.speed = rs.getInt("speed");
+                    cardChoice.stamina = rs.getInt("stamina");
+                    cardChoice.strength = rs.getInt("strength");
+                    cardChoice.willpower = rs.getInt("willpower");
+                    cardChoice.intelligence = rs.getInt("intelligence");
+                    cardChoice.hp = rs.getInt("hp");
+                    cardChoice.mood = rs.getInt("mood");
+                    cardChoice.skill_point = rs.getInt("skill_point");
+                    cardChoice.character_favor = rs.getInt("character_favor");
+                    cardChoice.event_condition = rs.getString("event_condition");
+                    cardChoice.is_random_value = rs.getBoolean("is_random_value");
+                    cardChoice.is_random_attribute = rs.getBoolean("is_random_attribute");
+                    cardChoice.is_random_block_training = rs.getBoolean("is_random_block_training");
+                    cardChoice.is_end_chained_event = rs.getBoolean("is_end_chained_event");
 
+                    cardChoices.add(cardChoice);
                 }
                 if (!hasData) {
                     System.out.println("查無資料");
@@ -252,20 +385,21 @@ public class DataCollection {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        return cardChoices;
     }
 
     // 獲取指定 CARD UMA ID 和等級的卡片資訊
     // 這個方法會查詢資料庫中的 card_info 表，根據 uma_id 和 level_name
-    public static void GetCardInfoByCardUmaIdAndLevel(int uma_id, String level_name) {
+    public static void GetCardInfoByCardUmaIdAndLevel(int uma_id) {
         // SQL 查詢語句
-        String sql = "SELECT * FROM card_info WHERE uma_id = ? AND level_name = ?;";
+        String sql = "SELECT * FROM card_info WHERE uma_id = ? AND level_name = \"MAX BREAK\";";
 
         try (
             Connection conn = DriverManager.getConnection(url, user, password);
             PreparedStatement stmt = conn.prepareStatement(sql);
         ) {
             stmt.setInt(1, uma_id);
-            stmt.setString(2, level_name);
 
             // 執行查詢
             try (ResultSet rs = stmt.executeQuery()) {
@@ -349,5 +483,81 @@ public class DataCollection {
         }
     }
 
-    
+    // 獲取指定 UMA ID 的隨機一個事件並傳回
+    public Event getRandomUmaEvent(int uma_id) {
+        random.setSeed(System.currentTimeMillis());
+        List<Event> events = GetUmaEventsById(uma_id);
+
+        // 如果沒有找到任何事件，則返回 null
+        if (events.isEmpty()) {
+            System.out.println("沒有找到任何事件");
+            return null;
+        }
+
+        while (true) {
+            // 隨機選擇一個事件
+            int randomIndex = random.nextInt(events.size());
+            Event randomEvent = events.get(randomIndex % events.size());
+
+            // 檢查是否已經處理過這個事件
+            if (!umaEventSet.contains(randomEvent.event_id)) {
+                // 如果沒有處理過，則將其加入已處理集合並返回
+                umaEventSet.add(randomEvent.event_id);
+                return randomEvent;
+            }
+        }
+    }
+
+    // 獲取指定 UMA ID 的隨機一個外出事件並傳回
+    public Event getRandomOutdoorEvent(int uma_id) {
+        random.setSeed(System.currentTimeMillis());
+        List<Event> outdoorEvents = GetUmaOutdoorEventsById(uma_id);
+
+        // 如果沒有找到任何外出事件，則返回 null
+        if (outdoorEvents.isEmpty()) {
+            System.out.println("沒有找到任何外出事件");
+            return null;
+        }
+
+        while (true) {
+            // 隨機選擇一個外出事件
+            int randomIndex = random.nextInt(outdoorEvents.size());
+            Event randomOutdoorEvent = outdoorEvents.get(randomIndex % outdoorEvents.size());
+
+            // 檢查是否已經處理過這個事件
+            if (!umaEventSet.contains(randomOutdoorEvent.event_id)) {
+                // 如果沒有處理過，則將其加入已處理集合並返回
+                umaEventSet.add(randomOutdoorEvent.event_id);
+                return randomOutdoorEvent;
+            }
+        }
+    }
+
+    // 獲取指定 UMA ID 的隨機一個卡片事件並傳回
+    public CardEvent getRandomCardEvent(int uma_id) {
+        random.setSeed(System.currentTimeMillis());
+        List<CardEvent> cardEvents = GetCardEventByCardUmaId(uma_id);
+
+        // 如果沒有找到任何事件，則返回 null
+        if (cardEvents.isEmpty()) {
+            System.err.println("沒有找到任何卡片事件");
+            return null;
+        }
+
+        while (true) {
+            // 隨機選擇一個卡片事件
+            int randomIndex = random.nextInt(cardEvents.size());
+            CardEvent randomCardEvent = cardEvents.get(randomIndex % cardEvents.size());
+
+            // 檢查是否已經處理過這個事件
+            if (!cardEventSet.contains(randomCardEvent.event_id)) {
+                // 如果沒有處理過，則將其加入已處理集合並返回
+                cardEventSet.add(randomCardEvent.event_id);
+                return randomCardEvent;
+            }
+        }
+    }
+
+
 }
+
